7c338e79887dd7a425d5ba2d53fcd6d4c60a952e
OmkarDeshpande13 <omkardeshpande59@gmail.com>
2026-01-04 14:41:31 +0530

    First Program

---
+++ JayGanesh.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+int main()
+{
+    printf("Jay Ganesh...");
+
+    return 0;
+}                       
+
+
+
+423
+
+433 codee convert into definatinn in =to out class
+
+
+
+
+
+
